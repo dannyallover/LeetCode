0eb2db36f678db82f71a27cd2fcee2a3452e7f9c
Danny Halawi <dannyhalawi15@gmail.com>
2018-07-16 12:09:38 -0500

    Update guess_number_higher_or_lower.cpp

--- 374. Guess Number/guess_number_higher_or_lower.cpp
+++ 374. Guess Number/guess_number_higher_or_lower.cpp
@@ -1,6 +1,7 @@
 /*
 Written by: dannyallover
 Date: 7/12/2018
+
 Description:
 We are playing the Guess Game. The game is as follows:
 I pick a number from 1 to n. You have to guess which number I picked.
@@ -9,9 +10,11 @@ You call a pre-defined API guess(int num) which returns 3 possible results (-1, 
 -1 : My number is lower
  1 : My number is higher
  0 : Congrats! You got it!
+ 
 Example:
 n = 10, I pick 6.
 Return 6.
+
 Runtime: My runtime beats 100% of cpp submissions
 */
 
