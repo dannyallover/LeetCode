33d7568751951b44a326dab235e4dcdcba676861
Danny Halawi <dannyhalawi15@gmail.com>
2018-07-24 20:07:49 -0500

    Create palindrome_number.cpp

---
+++ 9. Palindrome Number/palindrome_number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) {
+            return false;
+        }
+        int num = x;
+        int rev = 0;
+        while(num != 0) {
+            rev = rev * 10 + (num % 10);
+            num = num / 10;
+        }
+  
+        if(x == rev)
+            return true;
+        else
+            return false;
+    }
+};
