18d6db1052041a4bfd0594001145cfaf20369ee6
Danny Halawi <dannyhalawi15@gmail.com>
2018-07-24 20:10:48 -0500

    Update palindrome_number.cpp

--- 9. Palindrome Number/palindrome_number.cpp
+++ 9. Palindrome Number/palindrome_number.cpp
@@ -1,3 +1,31 @@
+/*
+Written by: dannyallover
+Date: 7/25/2018
+
+Description:
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+Input: 121
+Output: true
+Example 2:
+Input: -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+Input: 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+Time Complexity: O(N)
+Space Complexity: constant
+
+Runtime: My runtime beat 94.14% of c++ solutions.
+
+Note: The problem asks if we could solve it without converting the integer to a string? Since I did it this way,
+can I do it the other way around?
+*/
+
 class Solution {
 public:
     bool isPalindrome(int x) {
