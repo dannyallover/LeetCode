7ce2acd33a8316c7b0524d44bf5fd0390ea3002b
Danny Halawi <dannyhalawi15@gmail.com>
2018-08-08 13:21:27 -0500

    Create permutations_two.cpp

---
+++ 47. Permutations II/permutations_two.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        //find all permutations using backtracking, use erase/unique
+        //find all permutations using next_permutations, sort them/put them in a set
+        
+        vector<vector<int>> perms;
+        set<vector<int>> uniquePerms;
+        sort(nums.begin(), nums.end());
+        uniquePerms.insert(nums);
+        
+        while(next_permutation(nums.begin(), nums.end())) {
+            uniquePerms.insert(nums);
+        }
+        
+        for(vector<int> x: uniquePerms) {
+            perms.push_back(x);
+        }
+        
+        return perms;
+    }
+};
