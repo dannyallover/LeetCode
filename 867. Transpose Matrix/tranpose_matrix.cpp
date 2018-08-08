667f9df0b3c1c4b7636a02b3ef01ba5b91c0e613
Danny Halawi <dannyhalawi15@gmail.com>
2018-08-08 13:47:27 -0500

    Create tranpose_matrix.cpp

---
+++ 867. Transpose Matrix/tranpose_matrix.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    public int[][] transpose(int[][] A) {
+        int[][] A_transposed = new int[A[0].length][A.length];
+        for(int r = 0; r < A[0].length; r++) {
+            for(int c = 0; c < A.length; c++) {
+                A_transposed[r][c] = A[c][r];
+            }
+        }
+        return A_transposed;
+    }
+}
