a80ae2e948a3a395b16855e4b501486cb1cd4786
Danny Halawi <dannyhalawi15@gmail.com>
2018-08-08 13:38:54 -0500

    Create palindrome_linked_list.cpp

---
+++ 234. Palindrome Linked List/palindrome_linked_list.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(!head || !head->next)
+            return true;
+        
+        ListNode* end = head;
+        int length = 0;
+        while(end) {
+            length++;
+            end = end->next;
+        }
+        
+        //cout << length << endl;
+        
+        int half = (length + 1) / 2;
+        ListNode* mid = head;
+        while(half--) {
+            mid = mid->next;
+        }
+        
+        //cout << mid->val << endl;
+        
+        ListNode* flipped = mid;
+        ListNode* prev = NULL;
+        while(flipped) {
+            ListNode* next = flipped->next;
+            flipped->next = prev;
+            prev = flipped;
+            flipped = next;
+        }
+        
+        ListNode* tail = prev;
+        while(head && tail) {
+            if(head->val != tail->val) {
+                return false;
+            }
+            head = head->next;
+            tail = tail->next;
+        }
+        
+        return true;
+    }
+};
