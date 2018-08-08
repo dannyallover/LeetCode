fe8b5b6b95d4baa41e4e6f727047c657e52f484e
Danny Halawi <dannyhalawi15@gmail.com>
2018-08-08 13:11:54 -0500

    Create valid_parenthesis.cpp

---
+++ 20. Valid Parenthesis/valid_parenthesis.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char> openBrackets;
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                openBrackets.push(s[i]);
+                continue;
+            }
+            else if(openBrackets.empty()) {
+                return false;
+            }
+            else if(s[i] == ')') {
+                if(openBrackets.top() != '(')
+                    return false;
+                else
+                    openBrackets.pop();
+            }
+            else if(s[i] == '}') {
+                if(openBrackets.top() != '{')
+                    return false;
+                else
+                    openBrackets.pop();
+            }
+            else if(s[i] == ']') {
+                if(openBrackets.top() != '[')
+                    return false;
+                else
+                    openBrackets.pop();
+            }
+        }
+        
+        return openBrackets.empty();
+    }
+};
