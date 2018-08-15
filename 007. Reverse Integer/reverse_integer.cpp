bbf16977b5463105b1d6379748e26d0cfaef046e
Danny Halawi <dannyhalawi15@gmail.com>
2018-08-15 12:27:51 -0500

    Update reverse_integer.cpp

--- 007. Reverse Integer/reverse_integer.cpp
+++ 007. Reverse Integer/reverse_integer.cpp
@@ -17,7 +17,7 @@ Output: 21
 
 Time Complexity: O(N)
 Space Complexity: constant
-Runtime: My runtime beats 95.49% of c++ solutions.
+Runtime: My runtime beats 97.19% of c++ solutions.
 
 Note: Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1].
 For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
