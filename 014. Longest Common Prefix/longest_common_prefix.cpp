2e7eb1a283528e183af52fe64d56b31483f29e61
Danny Halawi <dannyhalawi15@gmail.com>
2018-08-15 13:12:44 -0500

    Update and rename 14. Longest Common Prefix/longest_common_prefix.cpp to 014. Longest Common Prefix/longest_common_prefix.cpp

--- 014. Longest Common Prefix/longest_common_prefix.cpp
+++ 014. Longest Common Prefix/longest_common_prefix.cpp
@@ -1,3 +1,25 @@
+/*
+Written by: dannyallover
+Date: 08/08/2018
+
+Description:
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+Example 1:
+Input: ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+Input: ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+Time Complexity: O(S) where S is the sum of the characters in all strings
+Space Complexity: O(1)
+
+Runtime: My runtime beats 97.92% of c++ solutions.
+*/
+
 class Solution {
 public:
     string getPrefix(string s1, string s2) {
