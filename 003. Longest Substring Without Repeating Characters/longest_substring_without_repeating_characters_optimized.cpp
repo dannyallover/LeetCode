0f43c6bae7eb35603280a8ee42247a59e3daebed
Danny Halawi <dannyhalawi15@gmail.com>
2018-08-15 12:21:34 -0500

    Update longest_substring_without_repeating_characters_optimized.cpp

--- 003. Longest Substring Without Repeating Characters/longest_substring_without_repeating_characters_optimized.cpp
+++ 003. Longest Substring Without Repeating Characters/longest_substring_without_repeating_characters_optimized.cpp
@@ -10,7 +10,7 @@ Given "abcabcbb", the answer is "abc", which the length is 3.
 Given "bbbbb", the answer is "b", with the length of 1.
 Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
-Time Complexity: O(N)
+Time Complexity: O(n)
 Space Complexity: O(min(m,n)), where m is length of the map and n is length of string
 
 Runtime: My runtime beats 66% of c++ solutions.
