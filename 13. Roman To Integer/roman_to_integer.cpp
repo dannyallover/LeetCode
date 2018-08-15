961072b02783aa602507ed27429a6697bc934963
Danny Halawi <dannyhalawi15@gmail.com>
2018-08-15 12:57:26 -0500

    Update roman_to_integer.cpp

--- 13. Roman To Integer/roman_to_integer.cpp
+++ 13. Roman To Integer/roman_to_integer.cpp
@@ -1,3 +1,21 @@
+/*
+Written by: dannyallover
+Date: ~06/01/2018
+
+Description:
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+Note: You may not slant the container and n is at least 2.
+
+Example:
+Input: [1,8,6,2,5,4,8,3,7]
+Output: 49
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Runtime: My runtime beats 95.79% of c++ solutions.
+*/
+
 class Solution {
 public:
     int value(char c) {
