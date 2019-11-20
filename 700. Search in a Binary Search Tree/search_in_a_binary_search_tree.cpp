f0a6787d814544e8c8a269af6c60f051595965d3
Danny Halawi <dannyhalawi15@gmail.com>
2019-11-20 11:35:29 -0800

    Create search_in_a_binary_search_tree.cpp

---
+++ 700. Search in a Binary Search Tree/search_in_a_binary_search_tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+            if(node->left)
+                q.push(node->left);
+            if(node->right)
+                q.push(node->right);
+            if(node->val == val)
+                return node;
+        }
+        return NULL;
+    }
+};
