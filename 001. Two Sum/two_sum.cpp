3b8a380a11a26994d8512e6279f5e4db5bbfd423
Danny Halawi <dannyhalawi15@gmail.com>
2020-06-13 19:41:20 -0700

    Update two_sum.cpp

--- 001. Two Sum/two_sum.cpp
+++ 001. Two Sum/two_sum.cpp
@@ -1,40 +1,26 @@
 /*
 Written by: dannyallover
-Date: 07/08/2018
-
+Date: 07/08/2020
 Description:
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 Example:
 Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
-
-Notes: unordered_map in c++ is implimented using hash table instead of tree based maps.
-
 Time Complexity: O(n)
 Space Complexity: O(n)
-
-Runtime: My runtime beats 99.88% of cpp submissions
+Runtime: 24ms, faster than 44.56% of c++ solutions.
 */
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> hash;
-        vector<int> res;
+        unordered_map<int, int> map;
         for(int i = 0; i < nums.size(); i++) {
-            int compliment = target - nums[i]; // this is the number we're searching for
-            if(hash.find(compliment) != hash.end()) {
-                res.push_back(hash[compliment]);
-                res.push_back(i);
-                return res;
-            }
-            else {
-                hash[nums[i]] = i; // put in the key,value pair
-            }
+            if(map.find(target-nums[i]) != map.end()) return {map[target-nums[i]], i};
+            map[nums[i]] = i;
         }
-        return res;
+        return {-1, -1};
     }
 };
