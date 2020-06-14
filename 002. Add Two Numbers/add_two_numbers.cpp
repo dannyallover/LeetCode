3fbe7e24bf354b5781e3ad38cd860151272f6550
Danny Halawi <dannyhalawi15@gmail.com>
2020-06-13 19:48:45 -0700

    Update add_two_numbers.cpp

--- 002. Add Two Numbers/add_two_numbers.cpp
+++ 002. Add Two Numbers/add_two_numbers.cpp
@@ -1,19 +1,22 @@
 /*
 Written by: dannyallover
 Date: 08/15/2018
+
 Description:
 You are given two non-empty linked lists representing two non-negative integers.
 The digits are stored in reverse order and each of their nodes contain a single digit.
 Add the two numbers and return it as a linked list.
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
 Example:
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
+
 Notes: I was actually asked this question during my Visa interview.
-Time Complexity: O(n)
+
+Time Complexity: O(N)
 Space Complexity: O(1)
-Runtime: My runtime beats 97.45% of cpp submissions
 */
 
 /**
